48aec6a8c443bf0b44bbbd92189ce4eb0f09ff67
Isuru Alahakoon <ialahakoo@gmail.com>
2026-04-08 00:53:05 +0530

    create facilities catalogue page with resource listing UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import P_login from './pages/P_login/P_login'
 import P_register from './pages/P_register/P_register'
+import I_FacilitiesCatalogue from './pages/I_FacilitiesCatalogue/i_FacilitiesCatalogue'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/login' element={<P_login />} />
           <Route path='/register' element={<P_register />} />
+          <Route path='/facilities' element={<I_FacilitiesCatalogue />} />
         </Routes>
       </Router>
     </div>

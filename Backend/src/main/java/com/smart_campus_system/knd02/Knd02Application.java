3f967d7ef8c20865579e524b8d6b857d1a1e0eb8
ChamodHimasha <chamodabayawardana2003@gmail.com>
2026-04-07 11:41:30 +0530

    Your message here

---
+++ Backend/src/main/java/com/smart_campus_system/knd02/Knd02Application.java
@@ -0,0 +1,13 @@
+package com.smart_campus_system.knd02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knd02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Knd02Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/smart_campus_system/knd02/Knd02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_campus_system.knd02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knd02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
